fd377b6925d7ae1f1fdc3a4355e73d453ac7755b
jv813yh <vendysp97@gmail.com>
2024-04-24 23:42:38 +0200

    Add project files.

---
+++ NavigationDrawerMenu/MyHamburgerMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace NavigationDrawerMenu
+{
+
+    public class MyHamburgerMenu : Control
+    {
+        public static readonly DependencyProperty IsOpenProperty = 
+            DependencyProperty.Register(nameof(IsOpen), typeof(bool), typeof(MyHamburgerMenu), new PropertyMetadata(false));
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsClosedProperty =
+            DependencyProperty.Register(nameof(IsClosed), typeof(bool), typeof(MyHamburgerMenu), new PropertyMetadata(false));
+
+        public bool IsClosed
+        {
+            get { return (bool)GetValue(IsClosedProperty); }
+            set { SetValue(IsClosedProperty, value); }
+        }
+
+        public static readonly DependencyProperty MenuContentProperty =
+            DependencyProperty.Register(nameof(MenuContent), typeof(object), typeof(MyHamburgerMenu), new PropertyMetadata(null));
+        public object MenuContent
+        {
+            get { return (object)GetValue(MenuContentProperty); }
+            set { SetValue(MenuContentProperty, value); }
+        }
+        static MyHamburgerMenu()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyHamburgerMenu), new FrameworkPropertyMetadata(typeof(MyHamburgerMenu)));
+        }
+    }
+}
---
+++ UIWPFDemo/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using UIWPFDemo.Windows;
+
+namespace UIWPFDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+            HamburgerMenuDemoWindow hamWindow = new HamburgerMenuDemoWindow();
+
+            hamWindow.Show();
+        }
+    }
+
+}
