969111912a9b0fdf968aa0371249c46d63c0ead5
jv813yh <vendysp97@gmail.com>
2024-04-27 15:29:17 +0200

    Styling the Dropdown content

--- NavigationDrawerMenu/MyHamburgerMenu.cs
+++ NavigationDrawerMenu/MyHamburgerMenu.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace NavigationDrawerMenu
 {
@@ -7,7 +8,7 @@ namespace NavigationDrawerMenu
     public class MyHamburgerMenu : Control
     {
         public static readonly DependencyProperty IsOpenProperty = 
-            DependencyProperty.Register(nameof(IsOpen), typeof(bool), typeof(MyHamburgerMenu), new PropertyMetadata(false));
+            DependencyProperty.Register(nameof(IsOpen), typeof(bool), typeof(MyHamburgerMenu), new PropertyMetadata(false, OnIsOpenPropertyChanged));
 
         public bool IsOpen
         {
@@ -15,25 +16,65 @@ namespace NavigationDrawerMenu
             set { SetValue(IsOpenProperty, value); }
         }
 
-        public static readonly DependencyProperty IsClosedProperty =
-            DependencyProperty.Register(nameof(IsClosed), typeof(bool), typeof(MyHamburgerMenu), new PropertyMetadata(false));
+        public static readonly DependencyProperty OpenCloseDurationProperty =
+            DependencyProperty.Register(nameof(OpenCloseDuration), typeof(Duration), typeof(MyHamburgerMenu), new PropertyMetadata(Duration.Automatic));
 
-        public bool IsClosed
+        public Duration OpenCloseDuration
         {
-            get { return (bool)GetValue(IsClosedProperty); }
-            set { SetValue(IsClosedProperty, value); }
+            get { return (Duration)GetValue(OpenCloseDurationProperty); }
+            set { SetValue(OpenCloseDurationProperty, value); }
         }
 
         public static readonly DependencyProperty MenuContentProperty =
-            DependencyProperty.Register(nameof(MenuContent), typeof(object), typeof(MyHamburgerMenu), new PropertyMetadata(null));
-        public object MenuContent
+            DependencyProperty.Register(nameof(MenuContent), typeof(FrameworkElement), typeof(MyHamburgerMenu), new PropertyMetadata(null));
+        public FrameworkElement MenuContent
         {
-            get { return (object)GetValue(MenuContentProperty); }
+            get { return (FrameworkElement)GetValue(MenuContentProperty); }
             set { SetValue(MenuContentProperty, value); }
         }
         static MyHamburgerMenu()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MyHamburgerMenu), new FrameworkPropertyMetadata(typeof(MyHamburgerMenu)));
+        }
+
+        private static void OnIsOpenPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is MyHamburgerMenu hamburgerMenu)
+            {
+                hamburgerMenu.OnIsOpenPropertyChanged();
+            }
+        }
+
+        private void OnIsOpenPropertyChanged()
+        {
+            if(IsOpen)
+            {
+                OpenMenuAnimation();
+            }
+            else
+            {
+                CloseMenuAnimnation();
+            }
+        }
+
+        private void CloseMenuAnimnation()
+        {
+            DoubleAnimation closeAnimation = new DoubleAnimation(0, OpenCloseDuration);
+            BeginAnimation(WidthProperty, closeAnimation);
+        }
+
+        private void OpenMenuAnimation()
+        {
+            double contentWidth = MeasureContentWidth();
+
+            DoubleAnimation openAnimation = new DoubleAnimation(contentWidth, OpenCloseDuration);
+            BeginAnimation(WidthProperty, openAnimation);
+        }
+
+        private double MeasureContentWidth()
+        {
+            MenuContent.Measure(new Size(MaxWidth, MaxHeight));
+            return MenuContent.DesiredSize.Width;
         }
     }
 }
---
+++ NavigationDrawerMenu/MyHamburgerMenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace NavigationDrawerMenu
+{
+    public class MyHamburgerMenuItem : RadioButton
+    {
+        static MyHamburgerMenuItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyHamburgerMenuItem), new FrameworkPropertyMetadata(typeof(MyHamburgerMenuItem)));
+        }
+    }
+}
